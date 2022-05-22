1a32b1f70cc17420c83e50b5fda4f9493cd38834
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-05-22 23:18:35 +0200

    update profile

--- src/components/landing-page/profile/Profile.tsx
+++ src/components/landing-page/profile/Profile.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import clsx from 'clsx';
 import Image from 'next/image';
-import { PropsWithChildren } from 'react';
 import { animated, useSpring } from 'react-spring';
-import ContactButton from '../ContactButton';
+import ContactButtons from '../ContactButtons';
 import styles from './Profile.module.css';
+import { IoIosArrowDown } from 'react-icons/io';
+import { useHasScroll } from '~/hooks/useHasScroll';
 
 type ProfileProps = {
   className?: string;
@@ -24,33 +26,77 @@ export const Profile = ({
 }: ProfileProps) => (
   <div
     className={clsx(
-      'flex justify-center sm:justify-between w-full items-center gap-6 sm:gap-12 flex-col-reverse sm:flex-row',
+      'relative flex justify-center sm:justify-between w-full items-center gap-6 sm:gap-12 flex-col-reverse sm:flex-row',
       { [styles.profileHeight]: fullScreenSize },
       className
     )}
   >
     <div className="flex flex-col gap-6 sm:gap-10 w-full">
       <div className="w-fit flex flex-col gap-2 relative">
-        <img src="/images/point-bg.svg" className={styles.pointBg} />
+        <img
+          src="/images/point-bg.svg"
+          alt="decoration point"
+          className={styles.pointBg}
+        />
         <p className="text-gradient-primary text-2xl font-bold relative right-2">
           {upperName}
         </p>
-        <Title>{name}.</Title>
+
+        <h1 className="header-h1-fluid font-bold text-gradient-primary">
+          {name}
+        </h1>
+        <p className="header-h2-fluid text-textSecondary">
+          <b className="text-textPrimary">{descriptionHighlight}</b>{' '}
+          {description}
+        </p>
       </div>
-      <p className="text-3xl sm:text-4xl text-textSecondary">
-        <b className="text-textPrimary">{descriptionHighlight}</b> {description}
-      </p>
-      <ContactButton />
+      <ContactButtons
+        activeLinks={['github', 'newsletter', 'linkedin', 'twitter']}
+      />
     </div>
     <div className="w-fit">
       <Picture />
     </div>
+    <ScrollMore />
   </div>
 );
 
+const ScrollMore = () => {
+  const hasScroll = useHasScroll();
+
+  const style = useSpring({
+    from: { transform: 'translateY(0px)', opacity: 0 },
+    to: { transform: 'translateY(20px)', opacity: 1 },
+    loop: hasScroll ? false : { reverse: true },
+    delay: 5000,
+    config: {
+      tension: 100,
+      friction: 12,
+      mass: 1.2,
+    },
+  });
+
+  if (hasScroll) {
+    return null;
+  }
+
+  return (
+    <div className="absolute hidden md:flex bottom-4 left-0 right-0 items-center flex-col">
+      <animated.div style={style}>
+        <IoIosArrowDown style={{ width: 40, height: 40 }} />
+      </animated.div>
+      <animated.div style={style}>
+        <IoIosArrowDown
+          style={{ width: 40, height: 40, position: 'relative', bottom: 30 }}
+        />
+      </animated.div>
+    </div>
+  );
+};
+
 const Picture = () => {
   const [style, api] = useSpring(() => ({
-    from: { transform: 'scale(0) rotate(-160deg)' },
+    from: { transform: 'scale(0.5) rotate(-20deg)' },
     to: { transform: 'scale(1) rotate(0deg)' },
     config: {
       tension: 100,
@@ -69,30 +115,10 @@ const Picture = () => {
       <Image
         width={400}
         height={400}
-        className={clsx('rounded-full', styles.picture)}
+        className={clsx('rounded-full')}
         src="/images/my-face.png"
         alt="face of melvyn"
       />
     </animated.div>
-  );
-};
-
-const Title = ({ children }: PropsWithChildren<{}>) => {
-  const style = useSpring({
-    from: { transform: 'scale(0)' },
-    to: { transform: 'scale(1)' },
-    config: {
-      tension: 100,
-      friction: 10,
-    },
-  });
-
-  return (
-    <animated.h1
-      style={style}
-      className="text-6xl sm:text-7xl font-bold text-gradient-primary"
-    >
-      {children}
-    </animated.h1>
   );
 };
