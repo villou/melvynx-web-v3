1155006818b5eab0731580f204f49e2b11383714
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-06-01 00:06:06 +0200

    add inView compoentn

--- src/components/hooks-based/InView.tsx
+++ src/components/hooks-based/InView.tsx
@@ -1,6 +1,32 @@
 // in progress
-export default function InView() {
-  // const ref = React.useRef<HTMLDivElement>(null);
 
-  return null;
+import clsx from 'clsx';
+import { cloneElement, RefObject, useRef } from 'react';
+import { useInView } from '~/hooks/useInView';
+
+export function InView({
+  children,
+}: {
+  children: (ref: RefObject<never>, inView: boolean) => JSX.Element;
+}): JSX.Element {
+  const ref = useRef<never>(null);
+  const inView = useInView(ref, { threshold: 0.5 });
+
+  return children(ref, inView);
+}
+
+export function FadeUpInView({
+  children,
+}: {
+  children: JSX.Element;
+}): JSX.Element {
+  const ref = useRef<HTMLElement>(null);
+  const inView = useInView(ref, { threshold: 0.5 });
+
+  return cloneElement(children, {
+    ref: ref,
+    className: clsx(children.props.className, 'fadeup-enter', {
+      'fadeup-enter-done': inView,
+    }),
+  });
 }
