44840e6ba45aae38dbe75bccd06df1e7ea3a0656
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-06-01 09:50:18 +0200

    update skills

--- src/components/landing-page/skills/SkillsSection.tsx
+++ src/components/landing-page/skills/SkillsSection.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { skillsData } from '../../base/skill/skill.data';
 import { SkillItem } from '../../base/skill';
 import { Section } from '../Section';
+import { FadeUpInView } from '~/components/hooks-based/InView';
 
 export function SkillsSection() {
   return (
     <div className="hidden sm:block w-full">
       <Section title="Skills">
         <div className="flex justify-between">
-          {skillsData.map((skill) => (
+          {skillsData.map((skill, i) => (
             <div key={skill.title}>
-              <h3 className="text-2xl font-bold">{skill.title}</h3>
+              <FadeUpInView delay={100 + i * 100}>
+                <h3 className="text-2xl font-bold">{skill.title}</h3>
+              </FadeUpInView>
               <ul className="flex flex-col gap-2 mt-4">
-                {skill.items.map((item) => (
-                  <SkillItem key={item.title} {...item}>
-                    {item.title}
-                  </SkillItem>
+                {skill.items.map((item, y) => (
+                  <FadeUpInView delay={120 + i * 100 + y * 10} key={item.title}>
+                    <span>
+                      <SkillItem {...item}>{item.title}</SkillItem>
+                    </span>
+                  </FadeUpInView>
                 ))}
               </ul>
             </div>
