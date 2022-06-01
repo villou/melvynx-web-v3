059ae683641c8c189ef0b09a98c8a3d57eba71cf
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-06-01 10:00:18 +0200

    Add FadeUP on Project Section

--- src/components/projects/ProjectsSection.tsx
+++ src/components/projects/ProjectsSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FadeUpInView } from '../hooks-based/InView';
 import { Section } from '../landing-page/Section';
 import { ProjectCard } from './project-card/ProjectCard';
 import { projectsData } from './projects.data';
@@ -7,12 +8,12 @@ export default function ProjectsSection() {
   return (
     <Section title="Projects">
       <div className="flex flex-wrap gap-8">
-        {projectsData.map((project) => (
-          <ProjectCard
-            key={project.title}
-            className="flex-1"
-            project={project}
-          />
+        {projectsData.map((project, i) => (
+          <FadeUpInView key={project.title} delay={100 * i}>
+            <div className="flex-1">
+              <ProjectCard key={project.title} project={project} />
+            </div>
+          </FadeUpInView>
         ))}
       </div>
     </Section>
