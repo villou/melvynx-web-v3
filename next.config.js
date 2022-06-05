1e85646bd669af96592598d09eb64c1e801a8ef2
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-06-05 14:28:43 +0200

    add redirect for tik

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/dev-res',
+        destination: '/dev-debutant-ressource',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
